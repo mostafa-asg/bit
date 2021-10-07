445d829cb4aa715dd5cb678a967616d6cc501a12
Mostafa Asgari <mostafa.asg@gmail.com>
2021-10-07 15:00:58 +0330

    Enhance doc for 'And' method

--- bitset.go
+++ bitset.go
@@ -174,6 +174,9 @@ func (set *Set) Cardinality() int {
 }
 
 // And performs a logical AND of this target bit set with the argument bit set.
+// This bit set is modified so that each bit in it has the value true if and only if
+// it both initially had the value true and the corresponding bit in the bit set
+// argument also had the value true.
 func (set *Set) And(otherSet *Set) *Set {
 	length := min(len(set.arr), len(otherSet.arr))
 
